6b98e3fc069dd653db4a79380881cda149d817f4
sptrp <vankap0n@gmail.com>
2020-07-12 19:38:57 +0200

    initial commit

--- comments/index.js
+++ comments/index.js
@@ -1,27 +1,31 @@
 const express = require('express');
-const { randomBytes } =  require('crypto');
+const { randomBytes } = require('crypto');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
-const commentsPostById = {
+const commentsByPostById = {};
 
-};
-
-app.get('/posts:/id/comments', (req, res) => {
-    res.send(comments);
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentsByPostById[req.params.id] || []);
 });
 
 app.post('/posts/:id/comments', (req, res) => {
     const commentId = randomBytes(4).toString('hex');
     const { content } = req.body;
 
-    const comments = commentsPostById[req.params.id] || [];
+    const comments = commentsByPostById[req.params.id] || [];
 
     comments.push({ id: commentId, content });
 
-    res.status(201).send(posts[id]);
+    commentsByPostById[req.params.id] = comments;
+
+    res.status(201).send(comments);
+
+    console.log('Posted Comment')
 });
 
 app.listen(4001, () => {
